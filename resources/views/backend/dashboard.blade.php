7dc9531e209d3f9824f3f39177da75fa0dfb6131
nyiyelin <nyiyelin4@gmail.com>
2021-05-13 00:56:35 +0630

    backend template

---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('backendtemplate')
+@section('content')
+
+<div class="content">
+<div class="row">
+  <div class="col-md-12">
+    <h3 class="">Dashboard</h3>
+  </div>
+</div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BackendController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('backendtemplate');
+// });
+
+
+
+// backend admin
+Route::get('dashboard',[BackendController::class,'dashboard'])->name('dashboard');
