585e873ad5e53bbbb67bb51f568b6bbf829e8e6a
ayechanoo <sandarmoon2361996@gmail.com>
2021-05-13 22:53:00 +0630

    seeding

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,5 +9,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Country extends Model
 {
     use HasFactory,SoftDeletes;
-    protected $fillable=['name'];
+    protected $fillable=['name','logo'];
 }
